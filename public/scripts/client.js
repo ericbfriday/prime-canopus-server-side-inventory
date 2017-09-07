8511b69c3ec8857acc5ec54f4cd32c70ee18f5b2
Millie Walsh <millie@primeacademy.io>
2017-09-07 14:57:14 -0500

    add post route and request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,9 +2,32 @@ console.log('js');
 
 function onReady() {
   console.log('ready!');
-  
+  $('#addInventory').on('click', addInventory);
+  // on ready get all inventory
   getInventory();
 };
+
+// event listeners
+function addInventory() {
+    // var to hold value from form
+    var itemToAdd = $('#item').val();
+    console.log('addInventory', itemToAdd);   
+
+    // var to holding data we want to send to the server
+    var objectToSend = {
+        item: itemToAdd
+    };
+
+    // build post request and send to server
+    $.ajax({
+        type: 'POST',
+        url: '/inventory',
+        data: objectToSend, // data hold value we want to send
+        success: function(serverResp) {
+            console.log(serverResp);
+        }
+    });
+}
 
 // ajax functions
 function getInventory() {
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
 // global inventory variable
 var inventory = ['the ring', 'trousers', 'buttons', 'hairy feet'];
 
+//middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
 // this serve all other client side files
 // client.js, jquery, css
 app.use(express.static('public')); // public folder
+
 
 app.get('/', function(req, res) {
     var indexPath = path.join(__dirname, './public/views/index.html');
@@ -18,6 +23,13 @@ app.get('/', function(req, res) {
 app.get('/inventory', function(req, res) {
      console.log('in get inventory route');
     res.send(inventory);
+});
+
+app.post('/inventory', function(req, res) {
+    var item = req.body.item;    
+    console.log('in post inventory route', item);
+    inventory.push(item);
+    res.sendStatus(201);
 });
 
 app.listen(3000, function() {
