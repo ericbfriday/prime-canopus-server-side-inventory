a7a5d43bb719928a04a25c8f9a5143fd09e83f16
Millie Walsh <millie@primeacademy.io>
2017-09-07 14:07:11 -0500

    serve index and client

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 // global inventory variable
 var inventory = ['the ring', 'trousers', 'buttons', 'hairy feet'];
+
+// this serve all other client side files
+// client.js, jquery, css
+app.use(express.static('public')); // public folder
+
+app.get('/', function(req, res) {
+    var indexPath = path.join(__dirname, './public/views/index.html');
+    console.log(indexPath);
+    res.sendFile(indexPath);
+});
 
 app.get('/inventory', function(req, res) {
      console.log('in get inventory route');
