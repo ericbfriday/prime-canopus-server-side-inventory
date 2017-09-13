2f7f9f290617adc32b463e85c6552e3534e3ea96
Millie Walsh <millie@primeacademy.io>
2017-09-13 11:08:19 -0500

    get inventory route connect to db

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,11 +1,32 @@
 var router = require('express').Router();
+var pool = require('../modules/pool');
 
 // global inventory variable
-var inventory = ['the ring', 'trousers', 'buttons', 'hairy feet'];
+// var inventory = ['the ring', 'trousers', 'buttons', 'hairy feet'];
 
 router.get('/', function(req, res) {
     console.log('in get inventory route');
-   res.send(inventory);
+    pool.connect(function(connectionError, client, done) {
+        // connectionError - if error occurrs connect to the db
+        // client is our worker to run our query
+        // done - function we will call to release the client
+        if(connectionError){
+            console.log(connectionError);
+            res.sendStatus(500);
+        } else {
+            // ask the client to run our query
+            // params 1. query itself, 2. is callback
+            client.query('SELECT * FROM inventory', function(queryError, resultObj) {
+                if(queryError){
+                    console.log(connectionError);
+                    res.sendStatus(500);
+                }else{
+                    console.log('resultObj ->', resultObj);
+                    res.send(resultObj.rows);
+                }  
+            });
+        }
+    });
 });
 
 router.post('/', function(req, res) {
